b6b5f59fe0879df791e54246f8476c785e5ecfd5
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-20 14:41:38 -0300

    Add example of ngFor
    Signed-off-by: Natalia Graciano <nataliavgraciano@gmail.com>

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.produtos = [
+      "mouse",
+      "teclado",
+      "cabo",
+      "font"
+    ]
   }
 
+  produtos: string[] = []
+  ngOnInit(): void { }
 }
